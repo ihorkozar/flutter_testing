1302eaac3234dbd82f8a8560c13e9517f09fde61
Ihor Kozar <iak@anahorets-iMac.local>
2022-07-06 14:33:27 +0300

    Initial commit

---
+++ test/news_change_notifier_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_testing/news_change_notifier.dart';
+
+void main(){
+  late NewsChangeNotifier newsChangeNotifier;
+
+  // setUp(() {
+  //   newsChangeNotifier = NewsChangeNotifier(_newsService);
+  // });
+}
